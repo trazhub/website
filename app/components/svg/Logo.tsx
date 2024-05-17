db2727ca82a7ffe64ba1ed1a71fcb7395a143dda
Garv <garvverma900@gmail.com>
2024-05-17 21:11:36 +0530

    mini

--- app/components/svg/Logo.tsx
+++ app/components/svg/Logo.tsx
@@ -10,7 +10,7 @@ const Logo: React.FC<LogoProps> = ({ width = 1920, height = 1080 }) => {
     return (
         <Image
             className={`w-${width} h-${height}`}
-            src="/okay1.png"
+            src="/okay3.png"
             alt="Logo"
             width={220}
             height={220}
